35d2c94397f9223f66491592d8a628a6d6a1f4f1
Mithun Halder <mithunrptc@gmail.com>
2018-11-03 15:21:39 +0600

    Creating array_pointer.c file

---
+++ array_pointer.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+
+    int ara[5] = {50, 60, 70, 80, 90};
+
+    printf("Value of Array: %d, %d, %d, %d, %d\n", ara[0], ara[1], ara[2], ara[3], ara[4]);
+
+    return 0;
+}
