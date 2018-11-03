62fa682848bdc2217f8df0072426dbd1e80f223b
Mithun Halder <mithunrptc@gmail.com>
2018-11-03 14:40:27 +0600

    Complete string_3.c code

--- string_3.c
+++ string_3.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 int main(){
-
+    int char_value(char a){
+        if(a >= 48 && a <= 57){
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+    char c = getchar();
+    printf("%d", char_value(c));
     return 0;
 }
