02a538a8f872fa5ba386b504fef9a10cc4f55202
Mithun Halder <mithunrptc@gmail.com>
2018-11-05 20:35:45 +0600

    Complete pointer_2.c file

--- pointer_2.c
+++ pointer_2.c
@@ -1,7 +1,11 @@
-#include <stdio.c>
+#include <stdio.h>
 
-int main()}{
-
+int main(){
+    int x = 10;
+    int *p;
+    p = &x;
+    printf("*p = %d\n", *p);
+    printf("Value of p is %p\n", p);
 
     return 0;
 }
